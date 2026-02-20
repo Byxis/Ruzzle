2ecf11f8b9d6bb6669f6b17884784aba751b0e22
alexis.serrano <alexis.serrano@etu.umontpellier.fr>
2026-02-20 12:19:34 +0100

    feat: add raylib basic integration

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use raylib::prelude::*;
+
+fn main() {
+    let (mut rl, thread) = raylib::init().size(640, 480).title("Ruzzle").build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, ruzzle!", 12, 12, 20, Color::BLACK);
+    }
+}
