70a5a866f193d1dcd537d457676e3b9115093292
Alessandra <alessandra.van-rossen-martinez@etu.umontpellier.fr>
2026-03-11 15:19:40 +0100

    feat: added a better way to implement cubes by a struct and an impl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use raylib::prelude::*;
 
+enum BlockType {
+    Fix,
+    Rotation,
+    All,
+}
+
 fn main() {
     let screen_width = 800;
     let screen_height = 450;
@@ -16,8 +22,6 @@ fn main() {
         45.0,
     );
 
-    let cube_position = Vector3::new(0.0, 0.0, 0.0);
-
     rl.set_target_fps(60);
 
     while !rl.window_should_close() {
@@ -25,11 +29,19 @@ fn main() {
 
         d.clear_background(Color::RAYWHITE);
 
+        let cubes = vec![
+            BlockPrefab::new(0.0, 0.0, 0.0, Color::RED),
+            BlockPrefab::new(2.0, 0.0, 0.0, Color::BLUE),
+            BlockPrefab::new(-2.0, 1.0, 0.0, Color::GREEN),
+        ];
+
         {
             let mut d3d = d.begin_mode3D(camera);
 
-            d3d.draw_cube(cube_position, 2.0, 2.0, 2.0, Color::RED);
-            d3d.draw_cube_wires(cube_position, 2.0, 2.0, 2.0, Color::MAROON);
+            for cube in cubes.iter() {
+                cube.draw(&mut d3d);
+            }
+
             d3d.draw_grid(10, 1.0);
         }
 
@@ -41,5 +53,41 @@ fn main() {
             Color::DARKGRAY,
         );
         d.draw_fps(10, 10);
+    }
+}
+
+// Block type avec sa fonction d'implementation
+struct BlockPrefab {
+    position: Vector3,
+    size: Vector3,
+    color: Color,
+    block_type: BlockType,
+}
+
+impl BlockPrefab {
+    fn new(x: f32, y: f32, z: f32, color: Color) -> Self {
+        Self {
+            position: Vector3::new(x, y, z),
+            size: Vector3::new(1.0, 1.0, 1.0),
+            color,
+            block_type: BlockType::All,
+        }
+    }
+
+    fn draw(&self, d: &mut RaylibMode3D<RaylibDrawHandle>) {
+        d.draw_cube(
+            self.position,
+            self.size.x,
+            self.size.y,
+            self.size.z,
+            self.color,
+        );
+        d.draw_cube_wires(
+            self.position,
+            self.size.x,
+            self.size.y,
+            self.size.z,
+            Color::BLACK,
+        );
     }
 }
