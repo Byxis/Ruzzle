b0a9ba0e71f61016dbd38af9a3bb095801a12ffd
AndreaAntoniali <andreaoreste.antoniali@gmail.com>
2026-03-17 11:34:02 +0100

    refactor: menu_manager.draw to handle drawing the different game states

--- src/menu.rs
+++ src/menu.rs
@@ -1,6 +1,7 @@
 use raylib::{ffi::KeyboardKey, RaylibHandle};
 use raylib::prelude::*;
 
+use crate::crab::Crab;
 pub enum Menu {
     Title,
     Select,
@@ -42,15 +43,14 @@ impl MenuManager {
     }
 
     pub fn update(&mut self, rl: &RaylibHandle) {
+        
         self.frame_count += 1;
-
         match self.current_menu {
             Menu::Title => {
                 if rl.is_key_pressed(KeyboardKey::KEY_ENTER) {
                     self.current_menu = Menu::Loading;
                 }
             }
-
             Menu::Select => {
                 let mouse_pos = rl.get_mouse_position();
 
@@ -89,4 +89,134 @@ impl MenuManager {
             }
         }
     }
+
+
+    pub fn draw(&self, mut d : &mut RaylibDrawHandle, crab : &Crab, camera: &Camera3D){
+
+        d.clear_background(Color::BLACK);
+        match self.current_menu {
+            Menu::Title => {
+                draw_text_center(
+                    &mut d,
+                    "Ruzzle",
+                    (SCREEN_HEIGHT as i32) / 2 - 60,
+                    50,
+                    Color::WHITE,
+                );
+                draw_text_center(
+                    &mut d,
+                    "Appuyez sur Entrée",
+                    (SCREEN_HEIGHT as i32) / 2,
+                    50,
+                    Color::WHITE,
+                );
+            }
+            Menu::Select => {
+
+
+                draw_text_center(
+                    &mut d,
+                    "RUZZLE",
+                    30,
+                    50,
+                    Color::WHITE,
+                );
+                // Bouton Play
+                d.draw_rectangle_rec(self.game_btn, Color::LIGHTGRAY);
+                let text_play = "Jouer";
+                let text_width_play = d.measure_text(text_play, 30);
+                d.draw_text(
+                    text_play,
+                    (self.game_btn.x + (200.0 - text_width_play as f32) / 2.0) as i32,
+                    (self.game_btn.y + (60.0 - 30.0) / 2.0) as i32,
+                    30,
+                    Color::BLACK,
+                );
+                // Bouton Settings
+                d.draw_rectangle_rec(self.settings_btn, Color::LIGHTGRAY);
+                let text_settings = "Options";
+                let text_width_settings = d.measure_text(text_settings, 30);
+                d.draw_text(
+                    text_settings,
+                    (self.settings_btn.x + (200.0 - text_width_settings as f32) / 2.0) as i32,
+                    (self.settings_btn.y + (60.0 - 30.0) / 2.0) as i32,
+                    30,
+                    Color::BLACK,
+                );
+                //Boutton Credit
+                d.draw_rectangle_rec(self.credit_btn, Color::LIGHTGRAY);
+                let text_settings = "Crédits";
+                let text_width_settings = d.measure_text(text_settings, 30);
+                d.draw_text(
+                    text_settings,
+                    (self.credit_btn.x + (200.0 - text_width_settings as f32) / 2.0) as i32,
+                    (self.credit_btn.y + (60.0 - 30.0) / 2.0) as i32,
+                    30,
+                    Color::BLACK,
+                );
+            }
+            Menu::Settings => d.draw_text("Settings Menu", 100, 100, 40, Color::DARKGRAY),
+            Menu::Game => {
+                {
+                    let mut d3d = d.begin_mode3D(camera);
+
+                    d3d.draw_grid(10, 1.0);
+                    crab.draw(&mut d3d);
+                }
+
+                let coordonnees = format!(
+                    "({:.2}, {:.2}, {:.2})",
+                    crab.position.x, crab.position.y, crab.position.z
+                );
+
+                d.draw_text(&coordonnees, 10, 40, 20, Color::DARKGRAY);
+                d.draw_fps(10, 10);
+            }
+
+            Menu::Loading => {
+                draw_text_center(
+                    &mut d,
+                    "Chargement...",
+                    (SCREEN_HEIGHT as i32) / 2 - 60,
+                    50,
+                    Color::WHITE,
+                );
+            }
+            Menu::Credit => {
+                draw_text_center(
+                    &mut d,
+                    "Jeu réalisé par :",
+                    (SCREEN_HEIGHT as i32) / 2 - 60,
+                    50,
+                    Color::WHITE,
+                );
+                draw_text_center(
+                    &mut d,
+                    "Alexey Serrané, Allessandraaaaaa, Carolayne, Max La Menax, André saitpascodé",
+                    (SCREEN_HEIGHT as i32) / 2,
+                    20,
+                    Color::WHITE,
+                );
+                                draw_text_center(
+                    &mut d,
+                    "Max La Menax, André saitpascodé",
+                    (SCREEN_HEIGHT as i32) / 2 +40,
+                    20,
+                    Color::WHITE,
+                );
+                
+            }
+        }
+    }
+}
+
+fn draw_text_center(d: &mut RaylibDrawHandle, text: &str, y: i32, font_size: i32, color: Color) {
+    let text_length = d.measure_text(text, font_size);
+    d.draw_text(
+        text,
+        (SCREEN_WIDTH as i32) / 2 - (text_length / 2),
+        y,
+        font_size,
+        color,
+    );
 }
