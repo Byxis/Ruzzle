7132a9d537f1a69d8fb3479d28b231b769fa01dc
Alessandra <alessandra.van-rossen-martinez@etu.umontpellier.fr>
2026-03-17 11:22:07 +0100

    feat: added the first input as test

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,13 @@ fn main() {
 
     rl.set_target_fps(60);
     let mut level = levels::level1::Level1::new();
-
     while !rl.window_should_close() {
         level.draw(&mut rl, &thread);
+        if rl.is_key_down(KeyboardKey::KEY_Q) {
+            println!("Rotate Left");
+        }
+        if rl.is_key_down(KeyboardKey::KEY_E) {
+            println!("Rotate Right")
+        }
     }
 }
