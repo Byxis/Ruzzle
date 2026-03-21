b1fee0908d0cd4ba1958e356e5630ddc9b6e3bd9
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-21 20:14:55 +0100

    fix:forgot the SoundManager import

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod crab_animator;
 use crate::crab::Crab;
 
 mod sound_manager;
+use crate::sound_manager::sound_manager::SoundManager;
 use crate::sound_manager::sound_manager::{BackgroundMusic, SoundEffect}; //access allowed for now (TODO : find a way to choose without making it public ?)
 
 const SCREEN_WIDTH: i32 = 1280;
