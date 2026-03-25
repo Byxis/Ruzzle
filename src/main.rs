19e200cb8a600e004a345904528b7f3d882e26ae
Caroline Floquet <floquet.caroline3010@gmail.com>
2026-03-25 12:32:26 +0100

    feat/sound-manager (#18)
    * fix:run correctly
    
    * fix:dl dependencies, runs correctly
    
    * feat:create sound_manager.rs and try playing sounds
    
    * fix:raylib function use, audio device and lifetimes
    
    * feat:added rsc/sounds folder and some sounds
    
    * fix:add update stream function for music
    
    * fix:sound is now playing correctly
    
    * fix:refactor and comments
    
    * feat:added click sound function
    
    * feat:added click sound file
    
    * feat:set default music volume
    
    * fix:make default volume as constant
    
    * feat:independent sound effect volume and default values
    
    * feat:added block rotation sound effect
    
    * fix:comments
    
    * fix:add reference for rotation sound
    
    * feat: add music file
    
    * feat:background music plays correctly
    
    * refactor:made file paths into enum
    
    * refactor:play_sound_effect instead of several play_<name>
    
    * feat:allow for track choice
    
    * feat:added sound effect cache for lazy loading
    
    * test:sound effect works on top of music
    
    * refactor:comments
    
    * refactor:moved sound_manager into folder for module separation
    
    * test:sound effect on 150th frame
    
    * fix:added _ match option for music
    
    * fix:added _ match option for sound effects
    
    * refactor:added a mod.rs file to reference sound_manager.rs
    
    * fix:forgot the SoundManager import
    
    * refactor:space and uppercase for comments in main.rs
    
    * refactor:space and uppercase for comments in sound_manager.rs
    
    * refactor:comments before functions
    
    * refactor:differentiate comments and doc, remove useless ones
    
    * refactor:cleaner comments in main.rs
    
    * refactor:single line import
    
    * refactor:removed useless comment
    
    * refactor:remove useless comments
    
    * test:removed test
    
    ---------
    
    Co-authored-by: Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
    Co-authored-by: Alexis <byxisgeek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ use crate::crab::crab::Crab;
 mod menu;
 use crate::menu::menu::MenuManager;
 
+mod sound_manager;
+use crate::sound_manager::sound_manager::{SoundManager, BackgroundMusic, SoundEffect};
+
+const SCREEN_WIDTH: i32 = 1280;
+const SCREEN_HEIGHT: i32 = 720;
+
 mod config;
 use config::Config;
 
@@ -38,7 +44,10 @@ fn main() {
         Vector3::new(0.0, 1.0, 0.0),
         45.0,
     );
-
+  
+    let audio = RaylibAudio::init_audio_device().expect("Failed to initialize audio device");
+    let mut sound_manager = SoundManager::new(&audio);
+  
     let spawn_point = Transform3D::new(Vector3::new(0.0, 5.0, 0.0), 0.0);
     let mut map = Map::new(&mut rl, &thread, "rsc/map.glb");
 
@@ -64,7 +73,15 @@ fn main() {
 
     rl.set_target_fps(60);
 
+    // Apply default sound parameters and start game music
+    sound_manager.set_background_music(&audio, BackgroundMusic::CrabRave);
+    sound_manager.start_background_music();
+
+    // Frame loop
     while !rl.window_should_close() {
+        // Update background music stream (for continuous playing)
+        sound_manager.update_music_stream(); 
+      
         //Updating the game
         menu_manager.update(&mut rl, &thread, &map, &mut crab, &camera);
 
---
+++ src/sound_manager/sound_manager.rs
@@ -0,0 +1,173 @@
+use std::collections::HashMap;
+
+use raylib::prelude::*;
+
+//------------------------------------------------------------------------------
+/// SOUND MANAGER
+/// This module manages all sound-related functionalities, including background music and sound effects.
+/// It provides functions to set and control background music, as well as to load and play cached sound effects on demand.
+//------------------------------------------------------------------------------
+
+/// SOUND EFFECTS AND BACKGROUND MUSIC ENUMS
+/// These enums define the available sound effects and background music tracks in the game, along with their file paths for loading.
+// TODO : make it into a config file ?
+
+/// Sound effects enum with file paths
+#[derive(PartialEq, Eq, Hash, Clone, Copy)] // Makes it usable in cache (as HashMap key)
+pub enum SoundEffect {
+    Boing,
+    Jump,
+    Walking,
+    Click,
+    Rotate,
+}
+
+impl SoundEffect {
+    fn path(&self) -> &'static str {
+        match self {
+            Self::Boing => "rsc/sounds/boing_effect.mp3", // To be decided
+            Self::Jump => "rsc/sounds/jump_effect.mp3", // For crab jump
+            Self::Walking => "rsc/sounds/walking_effect.mp3", // For crab movement
+            Self::Click => "rsc/sounds/click_effect.mp3", // For menu interactions
+            Self::Rotate => "rsc/sounds/rotate_effect.mp3", // For block rotation
+            _ => "rsc/sounds/boing_effect.mp3", // Boing is default sound effect
+        }
+    }
+}
+
+/// Background music enum with file paths
+pub enum BackgroundMusic {
+    CrabRave,
+    // TODO : other music choices ?
+}
+
+impl BackgroundMusic {
+    fn path(&self) -> &'static str {
+        match self {
+            Self::CrabRave => "rsc/sounds/8bit_crab_rave.mp3",
+            _ => "rsc/sounds/8bit_crab_rave.mp3", // Crab rave is default music
+            
+        }
+    }
+}
+
+/// DEFAULT VOLUME SETTINGS
+// TODO : also add it to config file ?
+const DEFAULT_MUSIC_VOLUME: f32 = 0.5;
+const DEFAULT_EFFECT_VOLUME: f32 = 0.5;
+
+/// SOUND MANAGER STRUCT
+/// This struct encapsulates all sound-related data and functionalities, 
+/// Including the current background music, volume levels, and a cache for sound effects to optimize loading and playback.
+pub struct SoundManager<'a> {
+    background_music: Option<Music<'a>>,
+    music_volume: f32,
+    pub music_playing: bool,
+    effects_cache: HashMap<SoundEffect, Sound<'a>>, // Cache for lazy loading and memory of sound effects
+    effect_volume: f32,
+}
+
+impl<'a> SoundManager<'a> {
+    pub fn new(audio: &'a RaylibAudio) -> Self {
+        SoundManager {
+            background_music: None, // Initialize later (allows for future track choice)
+            music_volume: DEFAULT_MUSIC_VOLUME,
+            effects_cache: HashMap::new(), // Initialize the effects cache
+            effect_volume: DEFAULT_EFFECT_VOLUME, // Volumes set to default
+            music_playing: false,
+        }
+    }
+
+    ///VOLUME CONTROLS
+
+    /// Sets the volume of the background music
+    /// f32 in [0.0 ; 1.0]
+    // TODO : make a slider for this thing
+    pub fn set_music_volume(&mut self, volume: f32) {
+        if let Some(ref mut music) = self.background_music {
+            music.set_volume(volume);
+        }
+        self.music_volume = volume;
+    }
+
+    // TODO : another slider for this one
+    // Sets the volume of all sound effects
+    // f32 in [0.0 ; 1.0]
+    pub fn set_effect_volume(&mut self, volume: f32) {
+        self.effect_volume = volume;
+    }
+
+    /// Sets default volumes for music and sound effects (acts as reset)
+    pub fn set_default_volumes(&mut self) {
+        self.set_music_volume(DEFAULT_MUSIC_VOLUME);
+        self.set_effect_volume(DEFAULT_EFFECT_VOLUME);
+    }
+
+    /// BACKGROUND MUSIC CONTROLS
+
+    /// Sets the background music to the specified track
+    pub fn set_background_music(&mut self, audio: &'a RaylibAudio, music: BackgroundMusic) {
+        self.background_music = Some(audio.new_music(music.path()).expect("Failed to load background music"));
+        self.background_music.as_mut().unwrap().set_volume(self.music_volume); // Maintain current volume when changing music
+        // Background music as Option needs unwrapping 
+    }
+
+    /// Launches background music if not already playing (only called once)
+    pub fn start_background_music(&mut self) {
+        if !self.music_playing {
+            self.background_music.as_mut().unwrap().play_stream();
+            self.music_playing = true;
+        }
+    }
+
+    /// Pauses background music (only if currently playing)
+    pub fn pause_background_music(&mut self) {
+        if self.music_playing {
+            self.background_music.as_mut().unwrap().pause_stream();
+            self.music_playing = false;
+        }
+    }
+
+    /// Resumes background music (only if currently paused)
+    pub fn resume_background_music(&mut self) {
+        if !self.music_playing {
+            self.background_music.as_mut().unwrap().resume_stream();
+            self.music_playing = true;
+        }
+    }
+
+    /// Updates the music stream (called every frame)
+    pub fn update_music_stream(&mut self) {
+        if let Some(ref mut music) = self.background_music {
+            music.update_stream();
+        }
+    }
+
+    /// SOUND EFFECT CONTROLS
+    /// To be added to actions that need a sound effect, directly add to action functions
+
+    /// Loads the specified sound effect and returns it (to be played immediately after)
+    /// Uses lazy loading with a cache to only load when needed and keep it in memory for future use
+    fn load_sound(&mut self, audio: &'a RaylibAudio, effect: SoundEffect) {
+        let effect_path = effect.path();
+        let new_sound: Sound<'a> = audio.new_sound(effect.path()).expect(&format!("Failed to load {effect_path}"));
+        self.effects_cache.insert(effect, new_sound);
+    }
+
+    /// Plays the specified sound effect from the cache at the current effect volume
+    /// Calls load_sound if the effect is not already in the cache
+    pub fn play_sound_effect(&mut self, audio: &'a RaylibAudio, effect: SoundEffect) {
+
+        if !self.effects_cache.contains_key(&effect) {
+            SoundManager::load_sound(self, audio, effect);
+        }
+
+        // Play sound effect from cache
+        let cached_effect = self.effects_cache.get(&effect).unwrap();
+        cached_effect.set_volume(self.effect_volume); // Set volume to current setting for effects
+        cached_effect.play();
+    }
+
+    
+
+}
