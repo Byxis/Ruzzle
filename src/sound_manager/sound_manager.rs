7c244c927d5df794b169a083dbd4e04c6fa5b6d5
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-26 08:35:40 +0100

    feat:updated sound manager as in test branch

--- src/sound_manager/sound_manager.rs
+++ src/sound_manager/sound_manager.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 
 use raylib::prelude::*;
 
+use crate::config::Config;
+
 //------------------------------------------------------------------------------
 /// SOUND MANAGER
 /// This module manages all sound-related functionalities, including background music and sound effects.
@@ -51,11 +53,6 @@ impl BackgroundMusic {
     }
 }
 
-/// DEFAULT VOLUME SETTINGS
-// TODO : also add it to config file ?
-const DEFAULT_MUSIC_VOLUME: f32 = 0.5;
-const DEFAULT_EFFECT_VOLUME: f32 = 0.5;
-
 /// SOUND MANAGER STRUCT
 /// This struct encapsulates all sound-related data and functionalities, 
 /// Including the current background music, volume levels, and a cache for sound effects to optimize loading and playback.
@@ -65,16 +62,20 @@ pub struct SoundManager<'a> {
     pub music_playing: bool,
     effects_cache: HashMap<SoundEffect, Sound<'a>>, // Cache for lazy loading and memory of sound effects
     effect_volume: f32,
+    default_config: &'a Config, // To access default values 
+    audio: &'a RaylibAudio, 
 }
 
 impl<'a> SoundManager<'a> {
-    pub fn new(audio: &'a RaylibAudio) -> Self {
+    pub fn new(audio: &'a RaylibAudio, config: &'a Config) -> Self {
         SoundManager {
             background_music: None, // Initialize later (allows for future track choice)
-            music_volume: DEFAULT_MUSIC_VOLUME,
+            music_volume: config.music_volume,
             effects_cache: HashMap::new(), // Initialize the effects cache
-            effect_volume: DEFAULT_EFFECT_VOLUME, // Volumes set to default
+            effect_volume: config.sound_effects_volume, // Volumes set to default
             music_playing: false,
+            default_config: config,
+            audio,
         }
     }
 
@@ -99,15 +100,15 @@ impl<'a> SoundManager<'a> {
 
     /// Sets default volumes for music and sound effects (acts as reset)
     pub fn set_default_volumes(&mut self) {
-        self.set_music_volume(DEFAULT_MUSIC_VOLUME);
-        self.set_effect_volume(DEFAULT_EFFECT_VOLUME);
+        self.set_music_volume(self.default_config.music_volume);
+        self.set_effect_volume(self.default_config.sound_effects_volume);
     }
 
     /// BACKGROUND MUSIC CONTROLS
 
     /// Sets the background music to the specified track
-    pub fn set_background_music(&mut self, audio: &'a RaylibAudio, music: BackgroundMusic) {
-        self.background_music = Some(audio.new_music(music.path()).expect("Failed to load background music"));
+    pub fn set_background_music(&mut self, music: BackgroundMusic) {
+        self.background_music = Some(self.audio.new_music(music.path()).expect("Failed to load background music"));
         self.background_music.as_mut().unwrap().set_volume(self.music_volume); // Maintain current volume when changing music
         // Background music as Option needs unwrapping 
     }
@@ -148,18 +149,18 @@ impl<'a> SoundManager<'a> {
 
     /// Loads the specified sound effect and returns it (to be played immediately after)
     /// Uses lazy loading with a cache to only load when needed and keep it in memory for future use
-    fn load_sound(&mut self, audio: &'a RaylibAudio, effect: SoundEffect) {
+    fn load_sound(&mut self, effect: SoundEffect) {
         let effect_path = effect.path();
-        let new_sound: Sound<'a> = audio.new_sound(effect.path()).expect(&format!("Failed to load {effect_path}"));
+        let new_sound: Sound<'a> = self.audio.new_sound(effect.path()).expect(&format!("Failed to load {effect_path}"));
         self.effects_cache.insert(effect, new_sound);
     }
 
     /// Plays the specified sound effect from the cache at the current effect volume
     /// Calls load_sound if the effect is not already in the cache
-    pub fn play_sound_effect(&mut self, audio: &'a RaylibAudio, effect: SoundEffect) {
+    pub fn play_sound_effect(&mut self, effect: SoundEffect) {
 
         if !self.effects_cache.contains_key(&effect) {
-            SoundManager::load_sound(self, audio, effect);
+            SoundManager::load_sound(self, effect);
         }
 
         // Play sound effect from cache
