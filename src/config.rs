a0f542138dad6acd1a6b239c5e960cde8aff2ba0
Caroline Floquet <caroline.floquet@etu.umontpellier.fr>
2026-03-26 08:20:24 +0100

    feat:updated config as in test branch

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
 pub struct Config {
     pub screen_width: i32,
     pub screen_height: i32,
+    pub sound_effects_volume: f32,
+    pub music_volume: f32,
 }
 /// Configuration files for the game
 /// #TODO : add the sound level and music level
@@ -9,6 +11,8 @@ impl Config {
         Config {
             screen_width: 1280,
             screen_height: 720,
+            sound_effects_volume: 0.5,
+            music_volume: 0.5,
         }
     }
     pub fn update(&mut self, new_screen_width: i32, new_screen_height: i32) {
