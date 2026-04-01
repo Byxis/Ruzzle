ec3b20cc79391851efd65cac1eae041b1a2d1677
Max Chateau <mx.chateau@axithem.fr>
2026-04-01 22:49:04 +0200

    fix: make a thread to avoid blocking if discord is not launched

--- src/discord/rpc.rs
+++ src/discord/rpc.rs
@@ -1,22 +1,24 @@
-use discord_rpc_client::{Client};
+use discord_rpc_client::Client;
+use std::thread;
 
-pub fn discord_rpc() -> Client {
-    let state_message = "Crab Rave";
+pub fn discord_rpc() -> Option<()> {
+    thread::spawn(|| {
+        let state_message = "Crab Rave";
 
-    // Create the client
-    let mut drpc = Client::new(1484565447046074459);
+        // Create the client
+        let mut drpc = Client::new(1484565447046074459);
 
-    // Register event handlers with the corresponding methods
-    drpc.on_ready(|_ctx| {
-        println!("ready?");
+        // Register event handlers with the corresponding methods
+        drpc.on_ready(|_ctx| {
+            println!("ready?");
+        });
+
+        // Start up the client connection
+        drpc.start();
+
+        // Set the activity - silently ignore if it fails
+        let _ = drpc.set_activity(|act| act.state(state_message));
     });
 
-    // Start up the client connection, so that we can actually send and receive stuff
-    drpc.start();
-
-    // Set the activity
-    drpc.set_activity(|act| act.state(state_message))
-        .expect("Failed to set activity");
-
-    return drpc;
+    None
 }
